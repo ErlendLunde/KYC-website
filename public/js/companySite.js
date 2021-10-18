5efc11163be5c3517b5c97e6af10610fb61cdab7
ErlendLunde <43610178+ErlendLunde@users.noreply.github.com>
2021-10-18 17:09:41 +0200

    Added working functionality to render data about company

--- public/js/companySite.js
+++ public/js/companySite.js
@@ -1,35 +1,95 @@
 console.log("client side javascript loaded")
-let orgInfo = {}
-const orgNr = "981078365"
 
-//Get and render data about the company 
-fetch("http://localhost:3000/companySearch?orgNr=" + encodeURIComponent(orgNr)).then((response)=>{
+const firstMessage = document.querySelector("#firstMessage")
+const secondMessage = document.querySelector("#secondMessage")
+const orgArticle = document.querySelector("#orgArticle")
+const dataForm = document.querySelector("form")
+const search = document.querySelector("input")
+const orgNr = search.value
+
+let orgInfo = {}
+//const orgNr = "981078365"
+
+dataForm.addEventListener("submit", (e)=>{
+    firstMessage.textContent = "Searching for company..."
+    secondMessage.textContent = "Doing PEP check for management of company..."
+    e.preventDefault()
+    console.log("search pressed")
+    console.log(search.value)
+
+    //Get and render data about the company
+    fetch("http://localhost:3000/companySearch?orgNr=" + encodeURIComponent(orgNr)).then((response)=>{
         response.json().then((data)=>{
+
             if(data.error){
-                console.log(error)
-            }else{
+                firstMessage.textContent = data.error
+            }else if(data.body.feilmelding){
+                firstMessage.textContent = "could not find company with given organization number"
+            }
+            else{
                 //Get relevant data
                 orgInfo = {
-                    Name: data.body.navn,
-                    Registered_in_Business_register: data.body.registrertIForetaksregisteret,
-                    Registered_as_a_charity: data.body.registrertIFrivillighetsregisteret,
-                    Registered_in_foundation_register: data.body.registrertIStiftelsesregisteret,
-                    Registered_in_VAT_register:data.body.registrertIMvaregisteret, 
-                    Undergoing_liquidation:data.body.underAvvikling, 
-                    Undergoing_forced_liquidation:data.body.underTvangsavviklingEllerTvangsopplosning
+                    name: data.body.navn,
+                    registered_in_Business_register: data.body.registrertIForetaksregisteret,
+                    registered_as_a_charity: data.body.registrertIFrivillighetsregisteret,
+                    registered_in_foundation_register: data.body.registrertIStiftelsesregisteret,
+                    registered_in_VAT_register:data.body.registrertIMvaregisteret, 
+                    undergoing_liquidation:data.body.underAvvikling, 
+                    undergoing_forced_liquidation:data.body.underTvangsavviklingEllerTvangsopplosning
                 }
+                //Look for dangerous data
+                if(orgInfo.undergoing_forced_liquidation || orgInfo.undergoing_liquidation || !orgInfo.registered_in_VAT_register ){
+                    //Render dangerous data
+                    const warning = document.createElement("p")
+                    warning.textContent = "Warning system picked up the following:"
+                    orgArticle.appendChild(warning)
+
+                    const flag = document.createElement("p")
+                    flag.className = "Flag"
+                    flag.textContent = "One or more hits found on following three atributes: \
+                    Undegoing forced liquidation: " + orgInfo.undergoing_forced_liquidation +" \
+                    Undergoing  liquidation: " + orgInfo.undergoing_liquidation + " \
+                    Not registrerd in VAT register: " + !orgInfo.registered_in_VAT_register
+                    orgArticle.appendChild(flag)
+
+                }else{
+                    const flag = document.createElement("p")
+                    flag.textContent = "No information flagged by system as bad"
+                    orgArticle.appendChild(flag)
+                }
+            
+                //Render what data has been checked
+                firstMessage.textContent ="Company found"
+                const orgH4 = document.createElement("h4")
+                orgH4.textContent = "Information checked by system:"
+                orgArticle.appendChild(orgH4)
+
+                for (const key in orgInfo){
+                    const pElement = document.createElement("p")
+                    const spanElement = document.createElement("span")
+                    orgArticle.appendChild(pElement)
+                    pElement.appendChild(spanElement)
+
+                    spanElement.textContent = key + ": "
+                    pElement.append(orgInfo[key])
+                
+                    
+                    
+                }
+
                 console.log(orgInfo)
 
             }
         })
-})
-
-//Get data about 'higher ups' and do a pep search on them and render data if a person show up on pepS 
-fetch("http://localhost:3000/companyHigerUps?orgNr=" + encodeURIComponent(orgNr)).then((response)=>{
+    })
+    fetch("http://localhost:3000/companyHigerUps?orgNr=" + encodeURIComponent(orgNr)).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
-                console.log(error)
-            }else{
+                secondMessage.textContent = data.error
+            }else if(data.body.feilmelding){
+                secondMessage.textContent = "could not do PEP check on company with given organization number"
+            }
+            else{
                 let employees  = []
                 console.log(data)
                 data.body.forEach(element => {
@@ -49,6 +109,7 @@ fetch("http://localhost:3000/companyHigerUps?orgNr=" + encodeURIComponent(orgNr)
                             if(data.error){
                                 console.log(data.error)
                             }
+                            //Possible to make it so that it only renders data if ther is a hit
                             else{
                                 console.log(element + " had "+ data.body.hits.length +" hits in PEP check")
                             }
@@ -58,3 +119,44 @@ fetch("http://localhost:3000/companyHigerUps?orgNr=" + encodeURIComponent(orgNr)
             }
         })
 })
+
+})
+
+
+
+
+//Get data about 'higher ups' and do a pep search on them and render data if a person show up on pepS 
+// fetch("http://localhost:3000/companyHigerUps?orgNr=" + encodeURIComponent(orgNr)).then((response)=>{
+//         response.json().then((data)=>{
+//             if(data.error){
+//                 console.log(error)
+//             }else{
+//                 let employees  = []
+//                 console.log(data)
+//                 data.body.forEach(element => {
+//                     element.roller.forEach(roleElement =>{
+//                         if(roleElement.person){
+//                             employees.push(roleElement.person.navn.fornavn + " " + roleElement.person.navn.etternavn)
+//                         }
+//                     })    
+//                 });
+
+//                 ///Remove duplicates as a person can have multiple jobs
+//                 employees = [...new Set(employees)]
+//                 //Continues with unique array
+//                 employees.forEach(element =>{
+//                     fetch("http://localhost:3000/pep?name=" + encodeURIComponent(element)).then((response)=>{
+//                         response.json().then((data)=>{
+//                             if(data.error){
+//                                 console.log(data.error)
+//                             }
+//                             //Possible to make it so that it only renders data if ther is a hit
+//                             else{
+//                                 console.log(element + " had "+ data.body.hits.length +" hits in PEP check")
+//                             }
+//                         })
+//                     })
+//                 })
+//             }
+//         })
+// })
